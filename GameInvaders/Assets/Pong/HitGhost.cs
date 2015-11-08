3a458ed0002470f80a414204fba6f206f7308d27
AlexandruLuncan18 <alex.luncan18@gmial.com>
2015-11-08 11:26:18 +0200

    Alex Pong

---
+++ GameInvaders/Assets/Pong/HitGhost.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitGhost : MonoBehaviour
+{
+    public static int TotalNumberOfGhosts; 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.name == "pacman")
+        {
+            TotalNumberOfGhosts = TotalNumberOfGhosts - 1;
+            if(TotalNumberOfGhosts == 0)
+            {
+                Application.LoadLevel("TheCar");
+            }
+            Debug.Log("Collider");
+            GameObject.Destroy(this.gameObject);
+        }
+        
+    }
+}
